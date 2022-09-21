d5c3f46e047af91fb33597c1f9b01df3b481a52a
shahroz.khan <shehroz.altaf92>
2022-09-21 16:39:37 +0500

    session timer

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8">
+    <meta charset="UTF-8">
+    <meta http-equiv="Cache-Control" content="no-store">
+    <title>SMK_ML Data Collection Applications Page</title>
+</head>
+
+<body cz-shortcut-listen="true">
+
+<h3>SMK_ML Apps</h3>
+
+<hr>
+<h5>SMK_ML Household Survey Line Listing App</h5>
+<p><a href="smkHhMl2022_2022_09_10_Rv1558.apk">Download and Install SMK_ML Line Listing App</a><span><small> Ver.2.28R.1558</small></span>
+</p>
+<hr>
+
+
+<h5>SMK_ML Data Collection App</h5>
+<p><a href="smkHhMl2022_2022_09_10_Rv1558.apk">Download and Install SMK_ML Data Collection App</a><span><small> Ver.2.28R.1558</small></span>
+</p>
+<hr>
+
+
+<h3>INSTRUCTIONS:</h3>
+<ul>
+    <li>Download and run the app file to install.</li>
+    <li>If asked to unblock, select 'Unblock'</li>
+    <li>'Open' the app.</li>
+    <li><span style="color:red">NEVER USE TEST ACCOUNT FOR ACTUAL DATA COLLECTION</span></li>
+</ul>
+
+
+</body>
+</html>
